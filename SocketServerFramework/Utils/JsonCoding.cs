c7405f4e67a2060b6112ef241d6e93342fb356ba
moonrailgun <moonrailgun@gmail.com>
2015-08-03 19:37:35 +0800

    JSON处理
    ADD JSON格式数据处理

---
+++ SocketServerFramework/Utils/JsonCoding.cs
@@ -0,0 +1,23 @@
+﻿using LitJson;
+
+namespace SocketServerFramework.Utils
+{
+    class JsonCoding<T>
+    {
+        /// <summary>
+        /// 将DTO编码成json数据
+        /// </summary>
+        public static string encode(T model)
+        {
+            return JsonMapper.ToJson(model);
+        }
+
+        /// <summary>
+        /// 将json数据解码成DTO
+        /// </summary>
+        public static T decode(string message)
+        {
+            return JsonMapper.ToObject<T>(message);
+        }
+    }
+}
